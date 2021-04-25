0125a22052f800abcc31b0327119b99f85e2fd23
Егор <en@welcomemedia.ru>
2021-04-25 23:29:15 +0300

    Add theme settings, signin page and signin modal

---
+++ src/components/ModalBlock/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+} from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+import { useStylesSignIn } from "../../pages/SignIn";
+
+interface ModalProps {
+  title: string;
+  children: React.ReactNode;
+  classes?: ReturnType<typeof useStylesSignIn>;
+  visible?: boolean;
+  onClose: () => void;
+}
+
+export const ModalBlock: React.FC<ModalProps> = ({
+  title,
+  onClose,
+  visible = false,
+  children,
+}: ModalProps): React.ReactElement => {
+  return (
+    <Dialog
+      open={visible}
+      onClose={onClose}
+      aria-labelledby='form-dialog-title'
+    >
+      <DialogTitle id='form-dialog-title'>
+        <IconButton onClick={onClose} color='secondary' aria-label='close'>
+          <CloseIcon style={{ fontSize: 26 }} color='secondary' />
+        </IconButton>
+        {title}
+      </DialogTitle>
+      <DialogContent>{children}</DialogContent>
+    </Dialog>
+  );
+};
+function useStyles() {
+  throw new Error("Function not implemented.");
+}
